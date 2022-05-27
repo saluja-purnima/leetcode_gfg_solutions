e174623dfd0c385542e90a98b2a7573ac73aac3b
PURNIMA SALUJA <89795591+saluja-purnima@users.noreply.github.com>
2022-05-27 16:36:49 +0530

    Time: 8 ms (8.42%), Space: 5.9 MB (75.58%) - LeetHub

---
+++ 258-add-digits/258-add-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int addDigits(int num) {
+         int sum=0, rem=0;
+        while(num>9){
+            
+     while(num>0){
+        
+         rem = num%10;
+         sum = sum+ rem;
+         num= num/10;
+     } 
+        num=sum;
+        sum=0;
+        }
+           return num;
+    }
+};
